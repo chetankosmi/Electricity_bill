65d4fd66773ac100ac246234a220368ae20227d1
chetankosmi <chetankosmi24@gmail.com>
2025-10-06 17:28:01 +0530

    save to new branch

--- Elec.py
+++ Elec.py
@@ -1,8 +1,13 @@
 units = float(input("Enter the number of units consumed: "))
 cost_per_unit = 5.0
 bill_amount = units * cost_per_unit
+discount = 0
+if bill_amount > 1000:
+    discount = bill_amount * 0.10  # 10% discount
+    bill_amount -= discount
 print("\nElectricity Bill")
 print("----------------")
 print(f"Units Consumed : {units}")
 print(f"Rate per Unit  : ₹{cost_per_unit}")
+print(f"Discount       : ₹{discount}")
 print(f"Total Bill     : ₹{bill_amount}")
